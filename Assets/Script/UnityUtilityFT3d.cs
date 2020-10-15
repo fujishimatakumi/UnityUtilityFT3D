e28b2c67bf4df701bd34c9fec45afc9467a7a637
hujishima takumi <hujishima.t425@gmail.com>
2020-10-15 21:00:33 +0900

    UnityUtility3dスクリプトを作成

---
+++ Assets/Script/UnityUtilityFT3d.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnityUtilityFT3d 
+{
+    /// <summary>
+    /// ゲームオブジェクトのマテリアルをセットします
+    /// セットするゲームオブジェクトにはメッシュレンダラーコンポーネントを追加してください
+    /// </summary>
+    /// <param name="obj">マテリアルを設定するゲームオブジェクト</param>
+    /// <param name="material">設定するマテリアル</param>
+    public static void SetObjectMaterial(GameObject obj,Material material)
+    {
+        MeshRenderer mr = obj.GetComponent<MeshRenderer>();
+        mr.material = material;
+    }
+}
